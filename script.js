b8f9b1a98365a169cdfa46269de7af60ac4340aa
Elijah Monjardin <monjardinelijah120@gmail.com>
2022-08-01 15:44:35 +0800

    Made form functional

--- script.js
+++ script.js
@@ -1,12 +1,33 @@
 let myLibrary = [];
-
-function Book(title, author, page, read) {
+let bookTitle,bookAuthor,bookPages,bookStatus;
+function Book(title, author, page, status) {
   this.title=title;
   this.author=author;
   this.page=page;
-  this.read=read;
+  this.status=status;
 }
 
 function addBookToLibrary() {
-  
+  bookTitle=document.getElementById("booktitle").value;
+  bookAuthor=document.getElementById("bookauthor").value;
+  bookPages=document.getElementById("bookpages").value;
+  bookStatus=document.querySelector('input[name="yesno"]:checked').value;
+  if((bookTitle=="")||(bookAuthor=="")||(bookPages=="")){
+    document.getElementById("errorMessage").innerHTML = "Please fill out all the fields.";
+  }else{
+    if (bookStatus=='yes'){
+      bookStatus=true;
+    }else{
+      bookStatus=false;
+    }
+    document.getElementById("errorMessage").innerHTML = "";
+    document.getElementById("booktitle").value = "";
+    document.getElementById("bookauthor").value = "";
+    document.getElementById("bookpages").value = "";
+    var newBook = new Book(bookTitle,bookAuthor,bookPages,bookStatus);
+    myLibrary.push(newBook);
+    console.table(myLibrary);
+  }
 }
+
+
